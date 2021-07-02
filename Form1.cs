d502db75cfa898f3aa91edf723cd5a88b1b0a048
Michał Madej <onyxia364@gmail.com>
2021-07-02 13:32:31 +0200

    Funkcja dodawania kosci

--- Form1.cs
+++ Form1.cs
@@ -12,6 +12,9 @@ namespace Projekt_Programowanie_WSIiZ
 {
     public partial class Form1 : Form
     {
+        List<Kostka> kosci = new List<Kostka>();
+        List<int> modyfikatory = new List<int>();
+
         public Form1()
         {
             InitializeComponent();
@@ -30,6 +33,73 @@ namespace Projekt_Programowanie_WSIiZ
         private void button2_Click(object sender, EventArgs e)
         {
             int xxxx;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            modyfikatory.Add(int.Parse(textBox1.Text));
+            kosci.Add(new Kostka(int.Parse(listBox1.SelectedItem.ToString())));
+            if (textBox1.Text == "0")
+                textBox2.Text = textBox2.Text + listBox1.SelectedItem.ToString() + ", ";
+            else if (int.Parse(textBox1.Text) > 0)
+                textBox2.Text = textBox2.Text + listBox1.SelectedItem.ToString() + "+" + textBox1.Text + ", ";
+            else
+                textBox2.Text = textBox2.Text + listBox1.SelectedItem.ToString() + textBox1.Text + ", ";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
